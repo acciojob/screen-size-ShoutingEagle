de95f4f813ddff5126c15c95a3b71f1e6e253bcc
ShoutingEagle <117582341+ShoutingEagle@users.noreply.github.com>
2023-08-01 10:40:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const sizeInfo = document.createElement("div");
+sizeInfo.id = "sizeInfo";
+document.body.appendChild(sizeInfo);
+const h1 = document.createElement("h1");
+sizeInfo.appendChild(h1);
+
+
+
+
+var refresh = setInterval(resolution,0);
+
+function resolution (){
+    var screenSize = window.screen;
+    console.log(screenSize);
+    h1.textContent = `Width: ${window.screen.width} and Height: ${window.screen.height}`
+}
