8387fd17e1d48007af2ecb51a82cacb63bb323cf
ShoutingEagle <117582341+ShoutingEagle@users.noreply.github.com>
2023-08-01 10:52:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //your JS code here. If required.
 const sizeInfo = document.createElement("div");
 sizeInfo.id = "sizeInfo";
+sizeInfo.style.height = "100vh";
+sizeInfo.style.width = "100vw";
 document.body.appendChild(sizeInfo);
 const h1 = document.createElement("h1");
 sizeInfo.appendChild(h1);
